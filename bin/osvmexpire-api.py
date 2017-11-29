d84eeac17db1837e2710129c5792a49695612b83
Olivier Sallou <olivier.sallou@gmail.com>
2017-11-29 14:16:20 +0100

    make port configurable with env variable OSVMEXPIRE_PORT

--- bin/osvmexpire-api.py
+++ bin/osvmexpire-api.py
@@ -6,6 +6,10 @@ from paste import httpserver
 
 
 def run():
+    vmexpire_port = '9311'
+    if 'OSVMEXPIRE_PORT' in os.environ:
+        vmexpire_port = os.environ['OSVMEXPIRE_PORT']
+
     prop_dir = 'etc/os-vm-expire'
     if not os.path.exists('/etc/os-vm-expire/osvmexpire-api-paste.ini'):
         application = deploy.loadapp(
@@ -15,8 +19,8 @@ def run():
         application = deploy.loadapp(
             'config:{prop_dir}/osvmexpire-api-paste.ini'.
             format(prop_dir=prop_dir), name='main', relative_to='/')
-    
-    httpserver.serve(application, host='0.0.0.0', port='9311')
+
+    httpserver.serve(application, host='0.0.0.0', port=vmexpire_port)
 
 if __name__ == '__main__':
     run()
